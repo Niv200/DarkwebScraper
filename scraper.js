3c9552ef52d5b774743816f56906f9a1ca116a41
Niv200 <Nivm200@gmail.com>
2021-06-09 03:55:21 +0300

    Scraping to json files
    Console gives an update once per hour on how much scrapes are done, and how many are new scrapes.

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 const colors = require("colors/safe");
 
@@ -28,58 +29,96 @@ const colors = require("colors/safe");
 	console.log(colors.green.bold("Using Tor. Continuing... "));
 
 	// Go to stronghold
-	await page.goto("http://nzxj65x32vh2fkhk.onion/all");
-	// await page.goto("http://nzxj65x32vh2fkhk.onion/pawpmwnog"); anonymous
-	// await page.goto("http://nzxj65x32vh2fkhk.onion/pfrk6vj35"); by antodb
-	let test = await page.evaluate(() => {
-		document
-			.querySelector(
-				"#list > div:nth-child(2) > div > div.pre-info.pre-header > div > div.col-sm-7.text-right > a"
-			)
-			.click();
-	});
+	let previousText = undefined;
+	let count = 0;
+	let scrapes = 0;
+	let newPosts = 0;
+	while (true) {
+		await page.goto("http://nzxj65x32vh2fkhk.onion/all");
+		// await page.goto("http://nzxj65x32vh2fkhk.onion/pawpmwnog"); anonymous
+		// await page.goto("http://nzxj65x32vh2fkhk.onion/pfrk6vj35"); by antodb
+		let test = await page.evaluate(() => {
+			document
+				.querySelector(
+					"#list > div:nth-child(2) > div > div.pre-info.pre-header > div > div.col-sm-7.text-right > a"
+				)
+				.click();
+		});
 
-	await page.waitForSelector(
-		"#show > div > div > div.well.well-sm.well-white.pre > div > ol > li > div",
-		{
-			visible: true,
-		}
-	);
-
-	let data = await page.evaluate(() => {
-		let topic = document.querySelector(
-			"#show > div > div > div.pre-info.pre-header > div > div.col-sm-5 > h4"
-		).innerHTML;
-		topic = topic.replaceAll("\t", "").replaceAll("\n", "");
-		let items = Array.from(
-			document.querySelectorAll(
-				"#show > div > div > div.well.well-sm.well-white.pre > div > ol > li > div"
-			)
+		await page.waitForSelector(
+			"#show > div > div > div.well.well-sm.well-white.pre > div > ol > li > div",
+			{
+				visible: true,
+			}
 		);
-		let newmap = items.map((item) => item.innerHTML);
-		newmap = newmap.filter((item) => !item.includes("&nbsp"));
-		let text = newmap.join("~");
-		text = "~" + text;
-		console.log(text);
-		let stamp = document
-			.querySelector(
-				"#show > div > div > div.pre-info.pre-footer > div > div:nth-child(1)"
-			)
-			.innerHTML.replaceAll("\t", "")
-			.replaceAll("\n", "");
-		let time = stamp.split(" at ")[1];
-		if (stamp.includes("</a>")) {
-			let newStamp = stamp
-				.replaceAll('">', "$")
-				.replaceAll("</a>", "$")
-				.split("$")[1];
-			stamp = newStamp;
+
+		let data = await page.evaluate(() => {
+			let topic = document.querySelector(
+				"#show > div > div > div.pre-info.pre-header > div > div.col-sm-5 > h4"
+			).innerHTML;
+			topic = topic.replaceAll("\t", "").replaceAll("\n", "");
+			let items = Array.from(
+				document.querySelectorAll(
+					"#show > div > div > div.well.well-sm.well-white.pre > div > ol > li > div"
+				)
+			);
+			let newmap = items.map((item) => item.innerHTML);
+			newmap = newmap.filter((item) => !item.includes("&nbsp"));
+			let text = newmap.join("~");
+			text = "~" + text;
+			console.log(text);
+			let stamp = document
+				.querySelector(
+					"#show > div > div > div.pre-info.pre-footer > div > div:nth-child(1)"
+				)
+				.innerHTML.replaceAll("\t", "")
+				.replaceAll("\n", "");
+			let time = stamp.split(" at ")[1];
+			if (stamp.includes("</a>")) {
+				let newStamp = stamp
+					.replaceAll('">', "$")
+					.replaceAll("</a>", "$")
+					.split("$")[1];
+				stamp = newStamp;
+			} else {
+				stamp = "Anonymous";
+			}
+			return { topic, stamp, time, text };
+		});
+		if (previousText != data.text) {
+			previousText = data.text;
+			console.log(data);
+			console.log(colors.green.bold("Scraped new post!"));
+			newPosts++;
+			createNewFile(data);
 		} else {
-			stamp = "Anonymous";
+			//Do when fail to scrape
 		}
-		return { topic, stamp, time, text };
-	});
-	console.log(data);
+		count++;
+		scrapes++;
+		if (count >= 60) {
+			console.log("---------------------------------");
+			console.log("1 hour status:");
+			console.log("Scraped for total of: " + scrapes + " times");
+			console.log("Of which " + newPosts + " new post(s) were scraped.");
+			console.log("---------------------------------");
+			count = 0;
+		}
+		await delay(60000);
+	}
 	// debugger;
 	// await browser.close();
 })();
+
+function delay(time) {
+	return new Promise(function (resolve) {
+		setTimeout(resolve, time);
+	});
+}
+
+function createNewFile(data) {
+	fs.readdir("posts/", (err, files) => {
+		console.log(files.length);
+		fs.writeFileSync("posts/" + files.length + ".json", JSON.stringify(data));
+	});
+}
